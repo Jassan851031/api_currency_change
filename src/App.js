870e8c1e622726f73652b807992fb7129cab203d
jassan leyva <jassan851031@gmail.com>
2020-02-16 00:06:55 -0300

    se agrega un filter y logo de bandera

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import CardContainer from './components/CardContainer';
 import Card from './components/Card';
 
@@ -7,22 +7,38 @@ import moment from 'moment';
 
 const App = () => {
 
+    // Esto se descomenta
     const initialState = useInitialState();
-    
+
+    const [filter, setFilter] = useState("");
+
+    const filterInputHandler = () => {
+        setFilter(() => document.getElementById("inputFilter").value);
+        console.log(initialState);
+    }
 
     return(
         <div className="App">
+
+            <div className="container mt-5">
+                <div className="input-group mb-3">
+                <div className="input-group-prepend">
+                    <span className="input-group-text" id="inputGroup-sizing-default">Filter</span>
+                </div>
+                    <input id="inputFilter" onChange={filterInputHandler} type="text" className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default"/>
+                </div>
+            </div>
+
             <CardContainer>
                 {
                     initialState != null &&
-                    Object.keys(initialState.quotes).map((key,i) =>
+                    Object.keys(initialState.quotes).map(key =>
                         <Card 
-                            key={i}
                             currSource={initialState.source} 
                             currDestiny={key.substr(3)} 
-                            date={ moment(initialState.timestamp*1000).format('MMMM Do YYYY, h:mm:ss a')} 
+                            date={ moment(initialState.timestamp * 1000).format('MMMM Do YYYY, h:mm:ss a')} 
                             value={initialState.quotes[key]}
-                            />
+                            display={filter}/>
                     )
                 }
             </CardContainer>
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
-
 import '../assets/styles/components/Card.css';
+import 'currency-flags/dist/currency-flags.min.css';
 
 /**
  * Tarjeta donde se mostrará la información
  * @author Jorge
  */
-const Card = ({ currSource, currDestiny, date, value }) => {
-    
+const Card = ({ currSource, currDestiny, date, value, display }) => {
     return(
+        (display.length < 1 || currDestiny.toLowerCase().includes(display.toLowerCase())) &&
         <div id={currDestiny} className="col-sm-3 col-md-3 pb-4">
           <div className="card card-outline-info">
             <div className="card-block">
@@ -18,6 +18,7 @@ const Card = ({ currSource, currDestiny, date, value }) => {
                         <h5 className="card-title d-flex justify-content-center">{value}</h5>
                         <p className="card-text">{date}</p>
                     </div>
+                    <div class={"currency-flag currency-flag-" + currDestiny.toLowerCase()}></div>
                 </div>
             </div>
           </div>
